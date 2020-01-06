becaaceae6f123cf5c6ca025e53632f5e01cf553
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-06 20:24:59 +0900

    add some codes for GCP pub/sub, subscriber

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/atrn0/rpi-gpio-subscriber
 
 go 1.13
+
+require (
+	cloud.google.com/go/pubsub v1.1.0
+	github.com/davecheney/gpio v0.0.0-20160912024957-a6de66e7e470 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"log"
+)
+
+const (
+	topicName = "hello_topic"
+	subName   = "sub_one"
+)
+
+func main() {
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, "project-id")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	topic, err := client.CreateTopic(ctx, topicName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	subConfig := pubsub.SubscriptionConfig{Topic: topic}
+	sub, err := client.CreateSubscription(ctx, subName, subConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
+		log.Printf("Got message: %s", m.Data)
+		m.Ack()
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = client.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	topic.Stop()
+}
