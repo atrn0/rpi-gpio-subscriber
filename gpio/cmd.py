aef56e80d0af3342f98f65089e8af85d9d482de4
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-09 18:28:14 +0900

    add sleep(2)

--- gpio/cmd.py
+++ gpio/cmd.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from gpiozero import LED
 
 
@@ -5,15 +7,18 @@ def push(pin, duration):
     print(f'push {pin}')
     pin = LED(pin)
     pin.blink(on_time=duration, n=1)
+    sleep(2)
 
 
 def on(pin):
     print(f'on {pin}')
     pin = LED(pin)
     pin.on()
+    sleep(2)
 
 
 def off(pin):
     print(f'off {pin}')
     pin = LED(pin)
     pin.off()
+    sleep(2)
