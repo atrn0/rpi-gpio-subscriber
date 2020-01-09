4e1e55eaef6346b52e3ee3a71624fe54b8a063d4
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-09 18:15:03 +0900

    fix gpiozero syntax

--- gpio/cmd.py
+++ gpio/cmd.py
@@ -3,14 +3,17 @@ from gpiozero import LED
 
 def push(pin, duration):
     print(f'push {pin}')
-    LED(pin).blink(on_time=duration, n=1)
+    pin = LED(pin)
+    pin.blink(on_time=duration, n=1)
 
 
 def on(pin):
     print(f'on {pin}')
-    LED(pin).on()
+    pin = LED(pin)
+    pin.on()
 
 
 def off(pin):
     print(f'off {pin}')
-    LED(pin).off()
+    pin = LED(pin)
+    pin.off()
