be88ba52ba6f2f02c7ab17807dac193eca64a43e
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-09 18:16:40 +0900

    pause

--- gpio/cmd.py
+++ gpio/cmd.py
@@ -1,3 +1,5 @@
+from _signal import pause
+
 from gpiozero import LED
 
 
@@ -5,15 +7,18 @@ def push(pin, duration):
     print(f'push {pin}')
     pin = LED(pin)
     pin.blink(on_time=duration, n=1)
+    pause()
 
 
 def on(pin):
     print(f'on {pin}')
     pin = LED(pin)
     pin.on()
+    pause()
 
 
 def off(pin):
     print(f'off {pin}')
     pin = LED(pin)
     pin.off()
+    pause()
