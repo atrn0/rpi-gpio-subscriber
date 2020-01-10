2f3f664cd02e78a27a82d9cb8d4aff43f3c2ae98
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-10 15:46:51 +0900

    change device setting

--- gpio/cmd_handler.py
+++ gpio/cmd_handler.py
@@ -3,7 +3,7 @@ from gpio.cmd import push, on, off
 
 def handle_cmd(cmd, pin):
     if cmd == 'push':
-        push(pin, 0.2)
+        push(pin, 0.5)
     elif cmd == 'on':
         on(pin)
     elif cmd == 'off':
